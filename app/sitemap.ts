b2304e62e3f243daecd2740d3a6fa319c76cf518
leynardoyosef <leynardoyosef@gmail.com>
2026-01-14 23:02:09 +0700

    feat(app): Restricted blacklist page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -36,12 +36,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/blacklist`,
-      lastModified: currentDate,
-      changeFrequency: 'weekly',
-      priority: 0.6,
-    },
-    {
       url: `${baseUrl}/contact`,
       lastModified: currentDate,
       changeFrequency: 'monthly',
